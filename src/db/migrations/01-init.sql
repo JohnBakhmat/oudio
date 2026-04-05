1556c6818f18b21d87525b2419255dab2e59c1b4
John Bakhmat <johnbakhmat@gmail.com>
2026-04-05 15:50:18 +0300

    feat: add file table

--- src/db/migrations/01-init.sql
+++ src/db/migrations/01-init.sql
@@ -1,52 +1,60 @@
-CREATE TABLE migrations (
-	id INTEGER PRIMARY KEY,
-	name TEXT NOT NULL UNIQUE,
-	applied_at DATE
+create table migrations (
+	id integer primary key,
+	name text not null unique,
+	applied_at date
 );
 
-CREATE TABLE album (
-	id TEXT PRIMARY KEY,
-	title TEXT NOT NULL UNIQUE,
-	mb_id TEXT UNIQUE,
-	mb_rg_id TEXT UNIQUE
+create table album (
+	id text primary key,
+	title text not null unique,
+	mb_id text unique,
+	mb_rg_id text unique
 );
 
-CREATE TABLE artist (
-	id TEXT PRIMARY KEY,
-	name TEXT NOT NULL UNIQUE,
-	mb_id TEXT UNIQUE,
-	acoust_id TEXT UNIQUE
+create table artist (
+	id text primary key,
+	name text not null unique,
+	mb_id text unique,
+	acoust_id text unique
 );
 
-CREATE TABLE track (
-	id TEXT PRIMARY KEY,
-	title TEXT NOT NULL,
-	track_number INTEGER NOT NULL DEFAULT 0,
-	mb_id TEXT,
-	album_id TEXT NOT NULL,
+create table track (
+	id text primary key,
+	title text not null,
+	track_number integer not null default 0,
+	mb_id text,
+	album_id text not null,
 
-	UNIQUE(title, track_number, album_id)
-	FOREIGN KEY(album_id) REFERENCES album(id) ON DELETE CASCADE
+	unique(title, track_number, album_id)
+	foreign key(album_id) references album(id) on delete cascade
 );
 
-CREATE INDEX track_album_idx ON track(album_id);
+create table file (
+	id text primary key,
+	path text not null unique,
+	track_id text not null unique,
 
-CREATE TABLE artist_track (
-	artist_id TEXT NOT NULL,
-	track_id TEXT NOT NULL,
-
-	PRIMARY KEY(artist_id, track_id),
-	FOREIGN KEY(artist_id) REFERENCES artist(id),
-	FOREIGN KEY(track_id) REFERENCES track(id)
+	foreign key (track_id) references track(id)
 );
 
-CREATE TABLE artist_album (
-	artist_id TEXT NOT NULL,
-	album_id TEXT NOT NULL,
+create index track_album_idx on track(album_id);
 
-	PRIMARY KEY(artist_id, album_id),
-	FOREIGN KEY(artist_id) REFERENCES artist(id),
-	FOREIGN KEY(album_id) REFERENCES album(id)
+create table artist_track (
+	artist_id text not null,
+	track_id text not null,
+
+	primary key(artist_id, track_id),
+	foreign key(artist_id) references artist(id),
+	foreign key(track_id) references track(id)
 );
 
-INSERT INTO migrations (name, applied_at) VALUES ("01-init", CURRENT_TIMESTAMP);
+create table artist_album (
+	artist_id text not null,
+	album_id text not null,
+
+	primary key(artist_id, album_id),
+	foreign key(artist_id) references artist(id),
+	foreign key(album_id) references album(id)
+);
+
+insert into migrations (name, applied_at) values ("01-init", current_timestamp);
